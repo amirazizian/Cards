e4fbd3706f6147da4eedd36dd1a879aeccd6ba72
Amirhossein Azizian <amir.azizian@gmail.com>
2020-08-15 06:07:01 +0000

    Initial sample files added

---
+++ deck.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <iostream>
+#include "deck.h"
+
+Deck::Deck()
+{
+}
+  
+Deck::~Deck()
+{
+}
+  
+void Deck::deal_hand(int sets, int cards)
+{
+}
+  
+void Deck::print_deck()
+{
+}
+
+void Deck::shuffle(int seed)
+{
+}
+
+void Deck::sort()
+{
+}
