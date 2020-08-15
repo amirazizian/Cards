8395ab77735384fcd887d8222ea6990440969c55
Amirhossein Azizian <amir.azizian@gmail.com>
2020-08-15 11:14:40 +0000

    Deal function added

--- deck.cpp
+++ deck.cpp
@@ -5,7 +5,8 @@
 
 Deck::Deck()
 {
-  for(int i = 0; i < 52; i++){
+  for(int i = 0; i < 52; i++)
+  {
     cards[i] = Card();
     cards[i].set_value(i % 13 + 1, i / 13);
   }
@@ -13,21 +14,33 @@ Deck::Deck()
   
 Deck::~Deck()
 {
-  for(int i = 0; i < 52; i++){
+  for(int i = 0; i < 52; i++)
+  {
     cards[i].~Card();
   }
 }
   
-void Deck::deal_hand(int sets, int cards)
+void Deck::deal_hand(int sets, int cardsPerSet)
 {
+  for(int i = 0; i < sets * cardsPerSet; i++)
+  {
+    cards[i].print();
+
+    if((i + 1) % cardsPerSet == 0)
+    {
+      cout << endl;
+    }
+  }
 }
   
 void Deck::print_deck()
 {
-  for(int i = 0; i < 52; i++){
+  for(int i = 0; i < 52; i++)
+  {
     cards[i].print();
 
-    if(i % 13 == 12){
+    if(i % 13 == 12)
+    {
       std::cout << endl;
     }
   }
