67509099495414ba77161775e168d286456f3ed7
Amirhossein Azizian <amir.azizian@gmail.com>
2020-08-15 07:11:07 +0000

    Deck has been initialized with cards, print method implemented in deck.

--- deck.cpp
+++ deck.cpp
@@ -4,10 +4,17 @@
 
 Deck::Deck()
 {
+  for(int i = 0; i < 52; i++){
+    cards[i] = Card();
+    cards[i].set_value(i % 13 + 1, i / 13);
+  }
 }
   
 Deck::~Deck()
 {
+  for(int i = 0; i < 52; i++){
+    cards[i].~Card();
+  }
 }
   
 void Deck::deal_hand(int sets, int cards)
@@ -16,6 +23,13 @@ void Deck::deal_hand(int sets, int cards)
   
 void Deck::print_deck()
 {
+  for(int i = 0; i < 52; i++){
+    cards[i].print();
+
+    if(i % 13 == 12){
+      std::cout << "\n";
+    }
+  }
 }
 
 void Deck::shuffle(int seed)
